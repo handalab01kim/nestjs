6c26002588fa4da85afd0ad5506cca2a7a2c3b5f
handalab01kim <yi.kim@handalab.com>
2025-06-05 14:02:25 +0900

    (react serve)

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -4,6 +4,15 @@ import { TransformDateInterceptor } from './interceptor/transform-date/transform
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // // React build 정적 파일 서빙
+  // app.use(express.static(join(__dirname, '..', 'public', 'build'))); 
+
+  // // React SPA 라우팅 처리 (없는 모든 경로는 index.html로 리다이렉트)
+  // app.get('*', (req, res) => {
+  //   res.sendFile(join(__dirname, '..', 'public', 'build', 'index.html'));
+  // });
+
   app.useGlobalInterceptors(new TransformDateInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
